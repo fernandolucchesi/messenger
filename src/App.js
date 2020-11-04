a04dde424df1cda5c54487643f3d5f35cb8d79f8
Fernando Alencar <f.alencar@sportradar.com>
2020-11-04 18:05:04 +0100

    Add project code formatting settings for vscode and app header.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-    <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight">
-        Tailwind and Create React App
-      </h1>
-      <p className="text-base text-gray-700 leading-normal">
-        Building apps together
-      </p>
+    <div className="max-w mx-auto flex p-6 bg-gray-100 shadow">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-indigo-700">Messenger App</h1>
+      </div>
     </div>
-  </div>
   );
 }
 
